83aeb71f1c51070f72cb7bce0a1a0529186d1bdb
Peter Tsoi <petertsoi@gmail.com>
2012-10-14 14:06:53 -0700

    Default Rails 3.1.0 setup
    Default Rails 3.1.0 setup with cs169 Gemfile for testing and heroku
    postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tanc::Application.config.secret_token = 'd0c4c001f79ff2949fa83ed51ec082b9a80cda802336fd3c9bd2547c6b12ab942b7675b88a46f1302538e07fc99b760a7bd4167c979c334eeef25c0a5bcac8ca'
