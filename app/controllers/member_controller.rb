8928f9d02e4406e17c862761bd2089ae78b14dd9
Peter Tsoi <petertsoi@gmail.com>
2012-10-21 17:17:52 -0700

    Add controller actions for signup & email confirm

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,8 +1,22 @@
 class MemberController < ApplicationController
   def signup
+    if params[:email]
+      if true #Member.create!(params[:email])
+        redirect_to("/member/confirm_account")
+      else 
+        flash[:error] = "Your account could not be created"
+      end
+    end
   end
   
   def confirm_account
+    if params[:code]
+      if true #Member.confirm(code)
+        redirect_to("/member/thanks")
+      else
+        flash[:error] = "Please click the confirmation link emailed to you"
+      end
+    end
   end
 
 end
