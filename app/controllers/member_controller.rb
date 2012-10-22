576b093799a76134df6cf3dfe0b87876e404fb69
Peter Tsoi <petertsoi@gmail.com>
2012-10-21 22:27:29 -0700

    Add email to the confirm url params

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,7 +1,7 @@
 class MemberController < ApplicationController
   def signup
     if params[:email]
-      if true #Member.create!(params[:email])
+      if Member.create(:email => params[:email], :status => "Pending", :type => "Mailing list")
         redirect_to("/member/confirm_account")
       else 
         flash[:error] = "Your account could not be created"
@@ -10,13 +10,13 @@ class MemberController < ApplicationController
   end
   
   def confirm_account
-    if params[:code]
-      if true #Member.confirm(code)
+    if params[:email] and params[:code]
+      thisUser = Member.find_by_email(params[:email])
+      if thisUser and thisUser.confirm(code)
         redirect_to("/member/thanks")
       else
         flash[:error] = "Please click the confirmation link emailed to you"
       end
     end
   end
-
 end
