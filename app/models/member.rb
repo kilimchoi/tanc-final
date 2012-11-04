174fa3df4d7bd750e705731d9466d7f9069f0af4
Ki Lim Choi <kl_choi@berkeley.edu>
2012-11-03 17:22:53 -0700

    added migration

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ActiveRecord::Base
-  attr_accessible :id, :email, :name, :password, :status, :type
+  attr_accessible :id, :email, :name, :password, :status, :type, :confirmed
   validates :email, presence: true, uniqueness: true, format: {with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i}
 
   def confirm(code)
