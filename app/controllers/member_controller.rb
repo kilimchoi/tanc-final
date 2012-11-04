7a172d9830937b7061cc61462227ec5240202a4a
Peter Tsoi <petertsoi@gmail.com>
2012-11-04 15:14:13 -0800

    Re-enable password saving

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -28,11 +28,15 @@ class MemberController < ApplicationController
     thisUser = Member.find_by_email(session[:user_email]) rescue nil
     @email = thisUser.email rescue nil    
     if params[:commit] == "Continue"
-       if params[:membership] == "member"
+      if thisUser and thisUser.update_password(params[:password], params["confirm-password"])
+        if params[:membership] == "member"
           redirect_to("/member/account_setup_member")
-       elsif params[:membership] == "non-member"
+        elsif params[:membership] == "non-member"
           redirect_to("/member/account_setup_non_member")
-       end        
+       end
+      else
+        flash[:error] = "Passwords must match and be more than 6 letters"        
+      end
     end
   end
 
