cf08c18352d07476c79d64a4209ca444f2056ced
Ki Lim Choi <kl_choi@berkeley.edu>
2012-11-05 17:58:30 -0800

    92.27% test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start do
+   add_filter '/vendor/'
+   add_filter '/features/'
+end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] = "test"
+ENV["RAILS_ENV"] ||= "test"
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
