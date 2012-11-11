9404a464d3adeff5b03526ed67b8053af0358c91
Ki Lim Choi <kl_choi@berkeley.edu>
2012-11-11 15:57:47 -0800

    adding cucumber tests for database management

--- features/step_definitions/member_steps.rb
+++ features/step_definitions/member_steps.rb
@@ -9,6 +9,19 @@ Given /^I logged in as "(.*?)" with password "(.*?)"$/ do |arg1, arg2|
   visit '/member/confirm_account?email=%s&code=%s' % [arg1, arg2]
 end
 
+Given /^I fill up all_member_info$/ do 
+   e-mail = member.email
+   pwd = member.password
+   first = member.first_name
+   last = member.last_name
+   edit = member.Edited
+   email.should == "kl_choi@berkeley.edu"
+   pwd.should == "secret_pwd"
+   first.should == "Tenzin"
+   last.should == "choi"
+   edit.should == "T"
+end
+
 Then /^I should receive a confirmation email$/ do
   email = ActionMailer::Base.deliveries.last
   email.subject.should == "Welcome to TANC"
