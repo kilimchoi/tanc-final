4557302576290a26d93d37baaf8eca2bbcf9f5de
Ki Lim Choi <kl_choi@berkeley.edu>
2012-12-05 12:07:20 -0800

    Aldi fixed logout problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def destroy
+    flash[:error] = "You are successfully logged out!"
+    redirect_to("/member")
+    session.delete(:user_email)#clear user data from session
+  end
+  def index
+  end 
 end
