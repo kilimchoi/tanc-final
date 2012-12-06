d18105a5b9b4def39c854a62129ce0b1e5a175d4
Ki Lim Choi <kl_choi@berkeley.edu>
2012-12-05 20:30:55 -0800

    adding state accessor added

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,7 @@
 class Member < ActiveRecord::Base
-  attr_accessible :email, :password, :status, :member_type, :first, :last, :admin, :telephone, :address1, :address2, :city, :zip, :gender, :year_of_birth, :country_of_birth, :occupation, :number_of_children, :payment_method
+  attr_accessible :email, :password, :status, :member_type, :first, :last, :admin, :telephone, :address1, :address2, :city, :zip, :gender, :year_of_birth, :country_of_birth, :occupation, :number_of_children, :payment_method, :state
   validates :email, presence: true, uniqueness: true, format: {with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i}
-
+  
   def self.random_password
     (0...8).map{65.+(rand(26)).chr}.join
   end
