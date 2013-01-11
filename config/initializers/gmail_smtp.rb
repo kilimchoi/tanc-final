0d3711ef8f5d1ecfaffeb174062e89c5aed60428
Ki Lim Choi <kl_choi@berkeley.edu>
2013-01-11 09:49:25 -0800

    adding

---
+++ config/initializers/gmail_smtp.rb
@@ -0,0 +1,1 @@
+require File.dirname(__FILE__) + '/gmail_smtp'
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,3 +1,8 @@
+# Rails plugin initialization.
+# You can also install it as a gem:
+#   config.gem "ambethia-recaptcha", :lib => "recaptcha/rails", :source => "http://gems.github.com"
+
+require File.dirname(__FILE__) + '/recaptcha' 
 ENV['RECAPTCHA_PUBLIC_KEY'] = '6Le4nNkSAAAAAO1rkbY0aFfeEGP4r7Gl4Yja6UgC'
 ENV['RECAPTCHA_PRIVATE_KEY'] = '6Le4nNkSAAAAADdi-Bk3_bdPV5X-IkEWfHgsAV7e'
 
